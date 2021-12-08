f1063515997d595d75ea7d922b54d95b4f9a076e
pes adam <pesadam15@gmail.com>
2021-12-08 21:22:07 +0100

    posielanie si esemesiek

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,31 @@ const socket = io.connect("http://localhost:5000");
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom]         = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if(username !== "" & room !== ""){
       socket.emit("join_room", room);
+      setShowChat(true);
     }
   };
 
   return (
     <div className="App">
-      <h3>Pripoj sa</h3>
-      <input placeholder="zadaj svoje meno" type="text" onChange={e => setUsername(e.target.value)}></input>
-      <input placeholder="Room id" onChange={e => setRoom(e.target.value)}></input>
-      <button onClick={joinRoom}>Pripoj sa</button>
-      <Chat socket={socket} username={username} room={room}/>
+      { !showChat ? (
+      
+      <div className="joinChatContainer">
+        <h3>Pripoj sa</h3>
+      
+        <input placeholder="zadaj svoje meno" type="text" onChange={e => setUsername(e.target.value)}></input>
+      
+        <input placeholder="Room id" onChange={e => setRoom(e.target.value)}></input>
+      
+        <button onClick={joinRoom}>Pripoj sa</button>
+      </div> )
+        : (
+        <Chat socket={socket} username={username} room={room}/>
+      )}
     </div>
   );
 }
--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from "react";
 
 function Chat({socket, username, room}) {
-
     const [currentMessage, setCurrentMessage] = useState("");
+    const [messageList, setMessageList] = useState([]);
+
     const SendMessage = async () => {
         if (currentMessage !== "") {
             const messageData = {
@@ -15,22 +16,30 @@ function Chat({socket, username, room}) {
                 
             };
         await socket.emit("send_message", messageData); 
+        setMessageList((list) => [...list, messageData]);
         }
     };
 
     useEffect( () => {
         socket.on("receive_message", (data) => {
-            console.log(data);
+            setMessageList((list) => [...list, data]);
         });
     }, [socket]);
 
     return(
-        <div>
+        <div className="chat-window">
             <div className="hat-header">
                 <p>Live Chat</p>
             </div>
 
-            <div className="chat-body"></div>
+            <div className="chat-body">
+                {
+                messageList.map((messageContent) => {
+                    return <h1>{messageContent.message}</h1>;
+                })
+                }
+
+            </div>
             
             <div className="chat-footer">
                 <input type="text" placeholder="tvoja sprava" onChange={e => setCurrentMessage(e.target.value)}></input>
